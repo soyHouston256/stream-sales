1c34c8b84241b7df353f71a1fccb877d1dab0834
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2025-11-15 01:32:59 -0500

    fix: postgres configuracion

--- prisma/migrations/20251115063039_init/migration.sql
+++ prisma/migrations/20251115063039_init/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT,
     "role" TEXT NOT NULL DEFAULT 'user',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
