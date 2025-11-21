9d8fc2e1e40948d6599773f1a470beb1aeb98f7a
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2025-11-20 21:17:47 -0500

    feat: deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable standalone output for Docker
+  output: 'standalone',
   images: {
     domains: [
       'streams-laboratory.s3.sa-east-1.amazonaws.com',
+      'stream-sales-dev-images-336912793236.s3.us-east-1.amazonaws.com',
       'vortex-streaming.com',
       'imgur.com',
       'i.imgur.com',
